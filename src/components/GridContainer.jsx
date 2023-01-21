816d7ebd33148c4ce3375b2596825f86b879886b
Anais <anais.lorteau@latelier.co>
2023-01-21 10:16:08 +0100

    improve grid container design

--- src/components/GridContainer.jsx
+++ src/components/GridContainer.jsx
@@ -1,22 +1,38 @@
 import React from "react";
 import "./../styles/components/GridContainer.scss";
 
-function GridContainer({ shows, selectShow, selectedShow}) {
+function GridContainer({ shows, selectShow, selectedShow }) {
   return (
     <ul className="gird-container">
       {shows.map((show) => (
         <li
           key={show.id}
-          className={`grid-container-item ${selectedShow === show.id ? 'selected' : ''}`}
+          className={`grid-container-item ${
+            selectedShow === show.id ? "selected" : ""
+          }`}
           onClick={() => selectShow(show.id)}
         >
+          <input
+            type="checkbox"
+            checked={selectedShow === show.id}
+            className="grid-container-item-checkbox checkbox-input"
+          />
+          {show.newShow && (
+            <div className="grid-container-item-new-tag">New</div>
+          )}
           <div className="grid-container-item-image-container">
             <img src="/assets/netflix-poster.jpeg" alt={show.name} />
           </div>
-
-          <h3>
-            {show.season} {show.name}
-          </h3>
+          <div className="grid-container-item-detail">
+            <h3>
+              {show.season} {show.name}
+            </h3>
+            {show.category && (
+              <div className="grid-container-item-detail-tag">
+                {show.category}
+              </div>
+            )}
+          </div>
         </li>
       ))}
     </ul>
